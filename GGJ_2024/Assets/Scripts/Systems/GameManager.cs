efe458bc70bf778abbd7af5ef2b154b511d9ec84
McKinley <caiohudson.red4@hotmail.com>
2024-01-28 14:05:45 -0300

    GameManager simples feita

--- GGJ_2024/Assets/Scripts/Systems/GameManager.cs
+++ GGJ_2024/Assets/Scripts/Systems/GameManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,17 +12,48 @@ public class GameManager : MonoBehaviour
     
     [SerializeField] 
     private CrowdController _crowdController;
-
+    [SerializeField] 
+    private Canvas retryCanvas;
+    
+    
     [SerializeField] 
     private BoolVariable gameStarted;
 
+    [SerializeField] 
+    private IntReference numberOfCollectables;
+
+    [SerializeField] 
+    private int maxCollectables = 5;
+    
+    [SerializeField] 
+    private BoolVariable goalReached;
     
     public void StartGame()
     {
         _inputManager.Pause = false;
-
     }
-    
-    
-    
+
+
+    private void Start()
+    {
+        StartCoroutine(CheckEndGame());
+    }
+
+    IEnumerator CheckEndGame()
+    {
+        yield return new WaitUntil(() => goalReached.Value || numberOfCollectables.Value >= maxCollectables);
+        Debug.Log("You Win!");
+        retryCanvas.gameObject.SetActive(true);
+        
+    }
+
+    public void EndGame()
+    {
+        _inputManager.Pause = true;
+        gameStarted.Value = false;
+        numberOfCollectables.Value = 0;
+        goalReached.Value = false;
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
